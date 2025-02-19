a4ee9e6e019147012f6ed8440a6007fbf0078288
Emily Arron <you@example.com>
2025-02-18 20:45:50 -0500

    PSET 1

--- Hi.py
+++ Hi.py
@@ -1,1 +1,19 @@
-print("hello")
+def welcome():
+    print("Welcome to the Hundred Acre Wood")
+    pass
+
+welcome()
+
+def greeting(name):
+    print("Welcome to The Hundred Acre Wood, " + name)
+    pass
+
+greeting("Alexis Covington")
+
+def print_catchphrase(character):
+    if character == "Pooh":
+        print("Oh bother")
+    elif character == "Tigger":
+        print("TTFN")
+
+print_catchphrase("Tigger")
